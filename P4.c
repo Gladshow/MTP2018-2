5b890366a194a2fdf408b82ce62e4221e7a23591
Gladshow <42741934+Gladshow@users.noreply.github.com>
2018-12-18 09:01:02 -0200

    Add files via upload

--- P4.c
+++ P4.c
@@ -1,32 +1,26 @@
-//Gladson Oliveira Silva  nº11811EEL002
-//P4
-
 #include <stdio.h>
 
-int recur(int m, int n);
+int recur(int m, int n)
+{
+	if(m == 0)
+	{
+		return n + 1; 
+	}
+	else if(m > 0 && n > 0)
+	{
+		recur(m - 1, recur(m, n - 1));
+	}
+	else if(m > 0 && n == 0)
+	{
+		recur(m - 1, 1);
+	}
+}
 
 int main()
 {
 	int m, n;
-	printf("DIGITE OS VALORES M, N, RESPECTIVAMENTE:\n");
-	scanf("%d%d", &m, &n);
-	printf("%d", recur(m,n));
+	printf("Digite os valores de m e n\n\n");
+	scanf("%i%i", &m,&n);
+	printf("TEMOS:\n %i", recur(m,n));
 	return 0;
-}
-
-int recur(m, n)
-{
-	if(m>0 && n==0)
-	{
-		recur(m-1,1);
-	}
-	if (m==0)
-	{
-		n++;
-		return n;
-	}
-	if (m>0 && n>0)
-	{
-		recur(m- 1, recur(m, n- 1));
-	}
 }
